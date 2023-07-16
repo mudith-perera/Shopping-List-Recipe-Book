2cd874470d62c38b7d1237280205a6b947eb483c
mudith-perera <makawitagemudith@gmail.com>
2023-07-16 16:51:58 +0530

    navigation is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Shopping-List-Recipe-Book';
+    title = 'Shopping-List-Recipe-Book';
+
+    loadedFeature = 'recipe';
+
+    onNavigate(feature: string) {
+        this.loadedFeature = feature;
+    }
 }
